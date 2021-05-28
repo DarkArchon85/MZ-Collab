60f487a71a33d75de11a3387c7a3aa94195d8533
SirLegna <sirlegnayt@gmail.com>
2021-05-28 19:10:26 -0400

    Before implementation of own pathfinding

--- LvMZ_SirLegna_GTBS.js
+++ LvMZ_SirLegna_GTBS.js
@@ -12,6 +12,17 @@ Imported["LvMZ_SirLegna_GTBS"] = true;
  * @author LordValinar, SirLegna
  * @url 
  *
+ * @param Movement Grid
+ *
+ * @param MovementGrid.maxSize
+ * @text Maximum Search Area for Grid
+ * @parent Movement Grid
+ * @desc Define the search limit for Game Characters when using Movement Grid in this plugin
+ * @default 0
+ * @type number
+ * @min 0
+ *
+ *
  * @help
  * ----------------------------------------------------------------------------
  * Instructions
@@ -62,6 +73,60 @@ const lvParams = PluginManager.parameters(pluginName);
 	plugin specific functions and classes
 ******************************************************************************/
 
+//-----------------------------------------------------------------------------
+// BattleGrid_Movement
+//
+// The game object class movement on the Battle Grid
+
+function BattleGrid_Movement() {
+    this.initialize(...arguments);
+}
+
+Object.defineProperties(BattleGrid_Movement.prototype, {
+	// unit
+	unit: {
+		get: function() {
+			return this._unit;
+		},
+		set:function (gameObj){
+			this._unit = gameObj;
+		}
+		configurable: true
+	},
+	// distance
+	distance: {
+		get: function() {
+			return this._dist;
+		},
+		set:function (dist){
+			this._dist = dist;
+		}
+		configurable: true
+	},
+});
+
+BattleGrid_Movement.prototype.initialize = function() {
+	this._unit = null;
+	this._dist = 0;
+	this._grid = []
+	this._movementSearchLimitFlag = false;
+};
+
+BattleGrid_Movement.prototype.setUp(unit, distance){
+	this._unit = unit;
+	this._dist = distance;
+}
+
+/*Creates a grid object based of the unit's position and max distance around them.
+Requirements for Unit:
+* Must have x and y parameters defined as TODO
+* Must have a movement function defined as TODO
+Returns a gridObject
+*/
+BattleGrid_Movement.prototype.calculateGrid = function(unit, distance){
+	
+}
+
 /******************************************************************************
 	rmmv_managers.js
 ******************************************************************************/
@@ -69,6 +134,14 @@ const lvParams = PluginManager.parameters(pluginName);
 /******************************************************************************
 	rmmv_objects.js
 ******************************************************************************/
+
+Game_Character.prototype.searchLimit = (function(){
+	var searchLimit = Game_Character.prototype.searchLimit;
+	return function(){
+		var originalResult = searchLimit.apply(this,arguments);
+		return lvParams["MovementGrid.maxSize"] || originalResult;
+	}
+})();
 
 /******************************************************************************
 	rmmv_scenes.js
